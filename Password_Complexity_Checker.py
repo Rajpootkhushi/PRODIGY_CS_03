6a83c1507bcaeaba6bf2e5ba16b210cd4d7509f6
Sakshi Banafar <149816747+Rajpootkhushi@users.noreply.github.com>
2025-02-27 10:30:48 +0530

    Update Password_Complexity_Checker.py

--- Password_Complexity_Checker.py
+++ Password_Complexity_Checker.py
@@ -1,17 +1,14 @@
 import re
 
 def assess_password_strength(password):
-    # Initialize criteria
     length_criteria = len(password) >= 8
     uppercase_criteria = re.search(r'[A-Z]', password) is not None
     lowercase_criteria = re.search(r'[a-z]', password) is not None
     number_criteria = re.search(r'[0-9]', password) is not None
     special_char_criteria = re.search(r'[!@#$%^&*(),.?":{}|<>]', password) is not None
 
-    # Count the number of criteria met
     criteria_met = sum([length_criteria, uppercase_criteria, lowercase_criteria, number_criteria, special_char_criteria])
 
-    # Determine strength based on criteria met
     if criteria_met == 5:
         strength = "Very Strong"
     elif criteria_met == 4:
@@ -23,7 +20,6 @@ def assess_password_strength(password):
     else:
         strength = "Very Weak"
 
-    # Provide feedback
     feedback = []
     if not length_criteria:
         feedback.append("Password should be at least 8 characters long.")
